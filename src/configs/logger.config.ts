1a61e6f672f7a23f983d771b04fbd842c8174816
Nikolay <bukharin17@gmail.com>
2026-02-01 17:20:35 +0400

    feat: enhance logger configuration with file-based rolling logs for production

--- src/configs/logger.config.ts
+++ src/configs/logger.config.ts
@@ -2,6 +2,7 @@ import { IncomingMessage } from 'node:http';
 
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModuleAsyncParams } from 'nestjs-pino';
+import path from 'path';
 
 import { UserAuthInfo } from '@/modules/auth/types';
 
@@ -10,11 +11,37 @@ export const getLoggerConfig = (): LoggerModuleAsyncParams => ({
   inject: [ConfigService],
   useFactory: (config: ConfigService) => {
     const isDev = config.get('NODE_ENV', 'development') === 'development';
+
     return {
       pinoHttp: {
         level: isDev ? 'debug' : 'info',
         autoLogging: true,
-        transport: isDev ? { target: 'pino-pretty' } : undefined,
+        transport: isDev
+          ? {
+              target: 'pino-pretty',
+              options: {
+                colorize: true,
+                singleLine: true,
+              },
+            }
+          : {
+              targets: [
+                { target: 'pino/file', options: { destination: 1 } },
+                {
+                  target: 'pino-roll',
+                  options: {
+                    file: path.join(
+                      config.get('LOGS_DIR_PATH', ''),
+                      'prod.log',
+                    ),
+                    frequency: 'daily',
+                    size: '20m',
+                    mkdir: true,
+                    extension: '.log',
+                  },
+                },
+              ],
+            },
         redact: {
           paths: ['req.headers.authorization'],
           censor: '[HIDDEN]',
